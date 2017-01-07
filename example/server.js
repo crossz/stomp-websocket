79c49456d2c4c0b51fc60ec8c11111f9c49606e3
crossz at home <drcross@outlook.com>
2017-01-07 14:15:49 +0800

    update: correct and log module for node.js

--- example/server.js
+++ example/server.js
@@ -1,8 +1,12 @@
-var connect = require('connect');
+var connect = require('connect'), serveStatic = require('serve-static'); 
+var logger = require('morgan');
+
+//var app = connect().use(logger());
+
 
 connect()
-  .use(connect.logger('dev'))
-  .use(connect.static(__dirname))
-  .use(connect.static(__dirname + '/../lib/'))
+  .use(logger())
+  .use(serveStatic(__dirname))
+  .use(serveStatic(__dirname + '/../lib/'))
   .listen(8080);
 console.log('Server running at http://0.0.0.0:8080/');
