2ee13c6bac96acd12cd3df5a9fc29531e14b5bfd
zheng <drcross@outlook.com>
2017-01-09 19:25:07 +0800

    correct: add a ws server in server.js with specific protocols.

--- example/server.js
+++ example/server.js
@@ -1,40 +1,26 @@
-var connect = require('connect'), serveStatic = require('serve-static'); 
+var connect = require('connect'), serveStatic = require('serve-static');
 var logger = require('morgan');
 
-var http = require("http")
-var ws = require("nodejs-websocket")
-var fs = require("fs")
-
-
-//var app = connect().use(logger());
-
-
 connect()
-  .use(logger())
-  .use(serveStatic(__dirname))
-  .use(serveStatic(__dirname + '/../lib/'))
-  .listen(8080);
+    .use(logger())
+    .use(serveStatic(__dirname))
+    .use(serveStatic(__dirname + '/../lib/'))
+    .listen(8080);
 console.log('Server running at http://0.0.0.0:8080/');
 
 
-var server = ws.createServer(function (connection) {
-	connection.nickname = null
-	connection.on("text", function (str) {
-		if (connection.nickname === null) {
-			connection.nickname = str
-			broadcast(str+" entered")
-		} else
-			broadcast("["+connection.nickname+"] "+str)
-	})
-	connection.on("close", function () {
-		broadcast(connection.nickname+" left")
-	})
-})
-server.listen(61623);
-console.log('Stomp server running at ws://0.0.0.0:61623/');
+var http = require("http");
+var ws = require("nodejs-websocket");
 
-function broadcast(str) {
-	server.connections.forEach(function (connection) {
-//		console.log(str)
-	})
-}
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer({"validProtocols":['v10.stomp', 'v11.stomp']},function (conn) {
+    console.log("New connection");
+    conn.on("text", function (str) {
+        console.log("Received "+str);
+        conn.sendText(str.toUpperCase()+"!!!")
+    });
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    });
+}).listen(61623);
+console.log('Stomp server running at ws://0.0.0.0:61623/');
